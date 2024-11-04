f9682042796eeca948cdfe7d32991e7701055078
Antonio Mellies <antoniomellies@gmail.com>
2024-11-04 01:03:05 -0300

    fix portainer port

--- scripts/install.sh
+++ scripts/install.sh
@@ -85,7 +85,7 @@ echo -e "\n\n"
 
 echo "############## Init - Install Portainer ###############";
 docker volume create portainer_data;
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest;
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest;
 PORTAINER_STATUS=$?;
 echo "############## End - Install Portainer ###############";
 echo -e "\n\n"
